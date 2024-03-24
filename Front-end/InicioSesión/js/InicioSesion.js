ff7e54926e4b39082edd688a6632ba8896cf6617
Niky <148483691+NikyMC@users.noreply.github.com>
2024-03-24 20:10:49 +0100

    WEB Inicio Sesion

---
+++ Front-end/InicioSesión/js/InicioSesion.js
@@ -0,0 +1,10 @@
+function iniciarSesion() {
+    let usuario = document.getElementById("user").value.trim(); // Obtener el valor del campo usuario
+    let contraseña = document.getElementById("pwd").value.trim(); // Obtener el valor del campo contraseña
+    
+    if (usuario === "" || contraseña === "") {
+        alert("¡Algún dato falta!");
+    } else {
+        alert("¡Has iniciado sesión con éxito!");
+    }
+}
